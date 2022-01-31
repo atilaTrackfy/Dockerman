c77530f408ade87cd6d63231ce5d3c10f28b35b0
Átila Saraiva Quintela Soares <atila.soares@trackfyapp.com>
2022-01-31 15:25:21 -0300

    removed host ip specification

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import time
 
 while True:
     message = "E ai 9vinhos"
-    r = requests.post("http://localhost:8888/", data={'data': message})
+    # r = requests.post("http://localhost:8888/", data={'data': message})
+    r = requests.post("http://dockerman-my-test.apps.13.68.137.63.nip.io/", data={'data': message})
     print(r.text)
     time.sleep(10)
